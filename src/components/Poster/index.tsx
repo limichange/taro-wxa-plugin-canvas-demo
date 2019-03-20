4126c5e241902bdb34ce78abee29fec1887ed6d8
limichange <limichange@hotmail.com>
2019-03-20 13:50:25 +0800

    Update

--- src/components/Poster/index.tsx
+++ src/components/Poster/index.tsx
@@ -1,6 +1,6 @@
 import { ComponentType } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Button, Text } from '@tarojs/components'
+import { View, Canvas, Text } from '@tarojs/components'
 import './index.less'
 const wechat = window['wx']
 
@@ -11,6 +11,12 @@ interface Poster {
 class Poster extends Component {
   factor
   ctx
+  drawArr
+
+  state = {
+    pxWidth: 0,
+    pxHeight: 0
+  }
 
   componentDidMount() {
     const sysInfo = window['wx'].getSystemInfoSync()
@@ -93,6 +99,159 @@ class Poster extends Component {
     if (text) {
       this.drawText(Object.assign(text, { x: textX, y: textY }))
     }
+  }
+
+  /**
+   * 画圆角矩形
+   */
+  _drawRadiusRect(x, y, w, h, r) {
+    const br = r / 2
+    this.ctx.beginPath()
+    this.ctx.moveTo(this.toPx(x + br), this.toPx(y)) // 移动到左上角的点
+    this.ctx.lineTo(this.toPx(x + w - br), this.toPx(y))
+    this.ctx.arc(
+      this.toPx(x + w - br),
+      this.toPx(y + br),
+      this.toPx(br),
+      2 * Math.PI * (3 / 4),
+      2 * Math.PI * (4 / 4)
+    )
+    this.ctx.lineTo(this.toPx(x + w), this.toPx(y + h - br))
+    this.ctx.arc(
+      this.toPx(x + w - br),
+      this.toPx(y + h - br),
+      this.toPx(br),
+      0,
+      2 * Math.PI * (1 / 4)
+    )
+    this.ctx.lineTo(this.toPx(x + br), this.toPx(y + h))
+    this.ctx.arc(
+      this.toPx(x + br),
+      this.toPx(y + h - br),
+      this.toPx(br),
+      2 * Math.PI * (1 / 4),
+      2 * Math.PI * (2 / 4)
+    )
+    this.ctx.lineTo(this.toPx(x), this.toPx(y + br))
+    this.ctx.arc(
+      this.toPx(x + br),
+      this.toPx(y + br),
+      this.toPx(br),
+      2 * Math.PI * (2 / 4),
+      2 * Math.PI * (3 / 4)
+    )
+  }
+
+  /**
+   * 计算文本长度
+   * @param {Array|Object}} text 数组 或者 对象
+   */
+  _getTextWidth(text) {
+    let texts: {
+      fontSize: number
+      text: string
+      marginLeft: number
+      marginRight: number
+    }[] = []
+
+    if (Object.prototype.toString.call(text) === '[object Object]') {
+      texts.push(text)
+    } else {
+      texts = text
+    }
+    let width = 0
+    texts.forEach(({ fontSize, text, marginLeft = 0, marginRight = 0 }) => {
+      this.ctx.setFontSize(this.toPx(fontSize))
+      width += this.ctx.measureText(text).width + marginLeft + marginRight
+    })
+
+    return this.toRpx(width)
+  }
+
+  /**
+   * 渲染一段文字
+   */
+  _drawSingleText({
+    x,
+    y,
+    fontSize,
+    color,
+    baseLine,
+    textAlign = 'left',
+    text,
+    opacity = 1,
+    textDecoration = 'none',
+    width,
+    lineNum = 1,
+    lineHeight = 0,
+    fontWeight = 'normal',
+    fontStyle = 'normal',
+    fontFamily = 'sans-serif'
+  }) {
+    this.ctx.save()
+    this.ctx.beginPath()
+    this.ctx.font =
+      fontStyle + ' ' + fontWeight + ' ' + this.toPx(fontSize, true) + 'px ' + fontFamily
+    this.ctx.setGlobalAlpha(opacity)
+    // this.ctx.setFontSize(this.toPx(fontSize));
+    this.ctx.setFillStyle(color)
+    this.ctx.setTextBaseline(baseLine)
+    this.ctx.setTextAlign(textAlign)
+    let textWidth = this.toRpx(this.ctx.measureText(text).width)
+    const textArr: string[] = []
+    if (textWidth > width) {
+      // 文本宽度 大于 渲染宽度
+      let fillText = ''
+      let line = 1
+      for (let i = 0; i <= text.length - 1; i++) {
+        // 将文字转为数组，一行文字一个元素
+        fillText = fillText + text[i]
+        if (this.toRpx(this.ctx.measureText(fillText).width) >= width) {
+          if (line === lineNum) {
+            if (i !== text.length - 1) {
+              fillText = fillText.substring(0, fillText.length - 1) + '...'
+            }
+          }
+          if (line <= lineNum) {
+            textArr.push(fillText)
+          }
+          fillText = ''
+          line++
+        } else {
+          if (line <= lineNum) {
+            if (i === text.length - 1) {
+              textArr.push(fillText)
+            }
+          }
+        }
+      }
+      textWidth = width
+    } else {
+      textArr.push(text)
+    }
+
+    textArr.forEach((item, index) => {
+      this.ctx.fillText(item, this.toPx(x), this.toPx(y + (lineHeight || fontSize) * index))
+    })
+
+    this.ctx.restore()
+
+    // textDecoration
+    if (textDecoration !== 'none') {
+      let lineY = y
+      if (textDecoration === 'line-through') {
+        // 目前只支持贯穿线
+        lineY = y
+      }
+      this.ctx.save()
+      this.ctx.moveTo(this.toPx(x), this.toPx(lineY))
+      this.ctx.lineTo(this.toPx(x) + this.toPx(textWidth), this.toPx(lineY))
+      this.ctx.setStrokeStyle(color)
+      this.ctx.stroke()
+      this.ctx.restore()
+    }
+
+    return textWidth
   }
 
   /**
@@ -202,7 +361,7 @@ class Poster extends Component {
   downloadResource(images = []) {
     const drawList: Promise<{}>[] = []
     this.drawArr = []
-    images.forEach((image, index) => drawList.push(help._downloadImageAndInfo(image, index)))
+    images.forEach((image, index) => drawList.push(this._downloadImageAndInfo(image, index)))
     return Promise.all(drawList)
   }
 
@@ -214,7 +373,7 @@ class Poster extends Component {
       const { x, y, url, zIndex } = image
       const imageUrl = url
       // 下载图片
-      this._downImage(imageUrl, index)
+      this._downImage(imageUrl)
         // 获取图片信息
         .then(imgPath => this._getImageInfo(imgPath, index))
         .then(({ imgPath, imgInfo }) => {
@@ -309,7 +468,7 @@ class Poster extends Component {
     return rpx * this.factor
   }
 
-  toRpx(px, int?:boolean) {
+  toRpx(px, int?: boolean) {
     if (int) {
       return parseInt((px * this.factor).toFixed(0))
     }
@@ -364,7 +523,6 @@ class Poster extends Component {
     ;(config.blocks || []).forEach(item => {
       heightArr.push(item.y + item.height)
     })
-
     ;(config.texts || []).forEach(item => {
       let height
       if (Object.prototype.toString.call(item.text) === '[object Array]') {
@@ -377,11 +535,9 @@ class Poster extends Component {
         heightArr.push(item.y + height)
       }
     })
-
     ;(config.images || []).forEach(item => {
       heightArr.push(item.y + item.height)
     })
-
     ;(config.lines || []).forEach(item => {
       heightArr.push(item.startY)
       heightArr.push(item.endY)
@@ -400,7 +556,7 @@ class Poster extends Component {
   }
 
   create(config) {
-    this.ctx = wx.createCanvasContext('canvasid', this)
+    this.ctx = wechat.createCanvasContext('canvasid', this)
 
     const height = this.getHeight(config)
     this.initCanvas(config.width, height, config.debug)
@@ -450,7 +606,7 @@ class Poster extends Component {
           }
         })
 
-        const res = wx.getSystemInfoSync()
+        const res = wechat.getSystemInfoSync()
         const platform = res.platform
         let time = 0
         if (platform === 'android') {
@@ -459,14 +615,16 @@ class Poster extends Component {
         }
         this.ctx.draw(false, () => {
           setTimeout(() => {
-            wx.canvasToTempFilePath(
+            wechat.canvasToTempFilePath(
               {
                 canvasId: 'canvasid',
                 success: res => {
-                  this.triggerEvent('success', res.tempFilePath)
+                  console.log('success', res.tempFilePath)
+                  // this.triggerEvent('success', res.tempFilePath)
                 },
                 fail: err => {
-                  this.triggerEvent('fail', err)
+                  console.log('fail', err)
+                  // this.triggerEvent('fail', err)
                 }
               },
               this
@@ -475,13 +633,24 @@ class Poster extends Component {
         })
       })
       .catch(err => {
-        wx.showToast({ icon: 'none', title: err.errMsg || '生成失败' })
+        wechat.showToast({ icon: 'none', title: err.errMsg || '生成失败' })
         console.error(err)
       })
   }
 
   render() {
-    return <View className='Poster' />
+    return (
+      <View className='container'>
+        <Canvas
+          canvasId='canvasid'
+          className="canvas"
+          style={{
+            width: `${this.state.pxWidth}px`,
+            height: `${this.state.pxHeight}px`
+          }}
+        />
+      </View>
+    )
   }
 }
 
